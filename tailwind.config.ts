c24809d350478ee05c3cd5bcc958a773c540bac8
maxim <bozzhik@ya.ru>
2024-09-22 17:28:09 +0300

    set `color` variables + typescript `alliases` + set up `tailwind` configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
 import type {Config} from 'tailwindcss'
+
+import plugin from 'tailwindcss/plugin'
 import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
   darkMode: ['class'],
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
+    screens: {
+      xl: {max: '1536px'},
+      lg: {max: '1280px'},
+      // md: {max: '1024px'},
+      sm: {max: '428px'},
     },
+    colors: {
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+      gray: {
+        DEFAULT: 'var(--gray)',
+        light: 'var(--gray-light)',
+        dark: 'var(--gray-dark)',
+      },
+      red: 'var(--red)',
+      blue: 'var(--blue)',
+    },
+    extend: {},
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [
+    plugin(function sizePlugin(api) {
+      api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
+    }),
+    tailwindcssAnimate,
+  ],
 }
 
 export default config
