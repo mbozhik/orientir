c117299881cc4e026c98523314b225e4f6babea8
maxim <bozzhik@ya.ru>
2024-09-23 15:52:03 +0300

    adapt `ProjectsModule` index sub-block

--- src/components/App/index/ProjectsModule.tsx
+++ src/components/App/index/ProjectsModule.tsx
@@ -12,10 +12,10 @@ import Text from '~/UI/Text'
 export default function ProjectsModule() {
   return (
     <section data-section="projects-index" className="relative grid place-items-center">
-      <Image className="object-cover w-full -z-10" src={ProjectsImage} alt="" />
+      <Image className="object-cover w-full -z-10 sm:h-[70vh]" src={ProjectsImage} alt="" />
 
-      <div className="absolute flex justify-between w-1/2 p-16 bg-background">
-        <div className="py-7 space-y-7">
+      <div className="absolute flex justify-between w-1/2 p-16 sm:flex-col-reverse sm:p-6 sm:gap-10 sm:w-auto sm:mx-3 xl:w-2/3 bg-background">
+        <div className="py-7 sm:py-0 space-y-7 sm:space-y-3">
           <Text type="sub" className="font-bold text-gray" text="1/4" />
 
           <Heading type="h2" text="Ориентир Запад" />
@@ -27,12 +27,12 @@ export default function ProjectsModule() {
           </Link>
         </div>
 
-        <div className="flex flex-col items-center justify-between">
-          <Image className="object-contain w-44" src={LogoImage} alt="" />
+        <div className="flex flex-col items-center justify-between sm:grid sm:grid-cols-2">
+          <Image className="object-contain w-44 sm:w-16" src={LogoImage} alt="" />
 
-          <div className="flex justify-between w-full">
-            <ArrowX className="scale-110" direction="left" />
-            <ArrowX className="scale-110" />
+          <div className="flex justify-between w-full sm:justify-end sm:gap-14">
+            <ArrowX className="scale-[1.1] sm:scale-[0.9]" direction="left" />
+            <ArrowX className="scale-[1.1] sm:scale-[0.9]" />
           </div>
         </div>
       </div>
