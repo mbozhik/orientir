87b7468f2b2ba85e801a5ea9c3009b021b3fca19
maxim <bozzhik@ya.ru>
2024-10-31 06:13:48 +0300

    ~ improve `Overview` projects block

--- src/components/App/projects/Overview.tsx
+++ src/components/App/projects/Overview.tsx
@@ -41,41 +41,49 @@ export default function Overview({items: projects}: {items: TProject[]}) {
 
       <div className="grid grid-cols-2 sm:flex sm:flex-col-reverse gap-6">
         <div className="space-y-2.5">
-          {filteredProjects.map((project, index) => (
-            <div className="space-y-4" key={project.id}>
-              <div className={cn('flex items-center justify-between px-6 py-5 text-background cursor-pointer hover:bg-red duration-200 group', activeTab === index ? 'bg-red' : 'bg-blue')} onClick={() => setActiveTab(activeTab === index ? null : index)}>
-                <Heading type="h2" text={project.division} />
-                <ArrowDownRight className={cn('scale-[1.3] xl:scale-100 fill-background group-hover:rotate-45 duration-200', activeTab === index && 'rotate-45')} />
-              </div>
+          {filteredProjects.map((project, index) => {
+            const filteredResidents = selectedState === 'Все' ? Object.values(project.residents) : Object.values(project.residents).filter((resident) => resident.status === selectedState)
 
-              {activeTab === index && (
-                <div className="grid grid-cols-2 sm:grid-cols-1 gap-8 sm:gap-10 pb-5">
-                  <div className="flex flex-col justify-between sm:gap-5">
-                    <Text type="h4" text={project.description} />
-                    <DetailsButton href={`/projects/${project.id}`} text="Подробнее" />
-                  </div>
+            return (
+              <div className="space-y-4" key={project.id}>
+                <div className={cn('flex items-center justify-between px-6 py-5 text-background cursor-pointer hover:bg-red duration-200 group', activeTab === index ? 'bg-red' : 'bg-blue')} onClick={() => setActiveTab(activeTab === index ? null : index)}>
+                  <Heading type="h2" text={project.division} />
+                  <ArrowDownRight className={cn('scale-[1.3] xl:scale-100 fill-background group-hover:rotate-45 duration-200', activeTab === index && 'rotate-45')} />
+                </div>
 
-                  <div className="mt-1 space-y-4">
-                    <Heading type="h3" text="Резиденты:" />
-                    <div className="space-y-1">
-                      {Object.values(project.residents).map(({name, status, type, area}) => (
-                        <div key={name} className="space-y-1.5 py-1.5 border-b-[1px] border-transparent hover:border-red cursor-pointer">
-                          <div className="flex justify-between">
-                            <Text type="sub" className="text-gray font-extralight" text={status} />
-                            {type && <Text type="sub" className="font-bold" text={type} />}
-                          </div>
-                          <div className="flex justify-between">
-                            <Text type="h4" className="font-bold" text={name} />
-                            <Text type="sub" className="text-gray font-extralight" text={`${area} м2`} />
-                          </div>
-                        </div>
-                      ))}
+                {activeTab === index && (
+                  <div className="grid grid-cols-2 sm:grid-cols-1 gap-8 sm:gap-10 pb-5">
+                    <div className="flex flex-col justify-between sm:gap-5">
+                      <Text type="h4" text={project.description} />
+                      <DetailsButton href={`/projects/${project.id}`} text="Подробнее" />
+                    </div>
+
+                    <div className="mt-1 space-y-4">
+                      <Heading type="h3" text="Резиденты:" />
+                      <div className="space-y-1">
+                        {filteredResidents.length === 0 ? (
+                          <Text type="h4" text="Нет резидентов для данного фильтра." />
+                        ) : (
+                          filteredResidents.map(({name, status, type, area}) => (
+                            <div key={name} className="space-y-1.5 py-1.5 border-b-[1px] border-transparent hover:border-red cursor-pointer">
+                              <div className="flex justify-between">
+                                <Text type="sub" className="text-gray font-extralight" text={status} />
+                                {type && <Text type="sub" className="font-bold" text={type} />}
+                              </div>
+                              <div className="flex justify-between">
+                                <Text type="h4" className="font-bold" text={name} />
+                                <Text type="sub" className="text-gray font-extralight" text={`${area} м2`} />
+                              </div>
+                            </div>
+                          ))
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              )}
-            </div>
-          ))}
+                )}
+              </div>
+            )
+          })}
         </div>
 
         <div className="relative overflow-hidden w-full bg-blue/25 aspect-[10/8]">
