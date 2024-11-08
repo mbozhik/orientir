a78fc3c1bbf440954fc3c4fdb830f7275ea33756
maxim <bozzhik@ya.ru>
2024-11-08 20:15:41 +0300

    finlize `ClientsModule` index sub-component

--- src/components/App/index/ClientsModule.tsx
+++ src/components/App/index/ClientsModule.tsx
@@ -1,13 +1,32 @@
 'use client'
 
+import {useEffect, useRef} from 'react'
+import {horizontalLoop} from '@/utils/gsap/horizontal-loop'
+
 import Image from 'next/image'
 
 export default function ClientsModule({items}: {items: string[]}) {
+  const marqueeRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (!marqueeRef.current) return
+
+    const marqueeItems = marqueeRef.current.querySelectorAll('[data-marquee="item"]')
+
+    horizontalLoop(marqueeItems, {
+      repeat: -1,
+      speed: 2,
+      paddingRight: 0,
+    })
+  }, [])
+
   return (
-    <div className="w-[75%] sm:w-full mx-auto border-t-[1px] border-gray-light">
-      <div className="flex items-center justify-around gap-5 sm:overflow-hidden sm:gap-10">
+    <div className="w-[75%] xl:w-full mx-auto border-t-[1px] border-gray-light">
+      <div ref={marqueeRef} className="relative flex overflow-hidden">
         {items.map((logo, index) => (
-          <Image className="object-contain s-32 hover:scale-[1.1] duration-300" key={index} src={logo} alt="" />
+          <div data-marquee="item" className="w-[350px] xl:w-[300px] sm:w-[200px] py-8 shrink-0 flex items-center justify-center" key={index}>
+            <Image className="w-40 xl:w-36 object-contain hover:scale-[1.1] duration-300" src={logo} alt={`Client logo ${index + 1}`} width={150} height={50} />
+          </div>
         ))}
       </div>
     </div>
