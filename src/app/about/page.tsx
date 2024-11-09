853e73798b07fbe98b9627f3d91a35bad93e5d57
maxim <bozzhik@ya.ru>
2024-11-09 14:28:16 +0300

    make `Quote` about block

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
+import Container from '~/Global/Container'
+
 import Hero from '~~/about/Hero'
+import Quote from '~~/about/Quote'
 
 export default function AboutPage() {
   return (
     <>
       <Hero />
+      <Container className="space-y-36 xl:space-y-28 sm:space-y-20 my-36 xl:my-28 sm:my-16">
+        <Quote />
+      </Container>
     </>
   )
 }
