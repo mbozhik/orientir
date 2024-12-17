759107eb8463c202defc3454b6656e4bb0b540d0
maxim <bozzhik@ya.ru>
2024-12-17 13:27:54 +0300

    redesign `Footer` Global component

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -24,23 +24,23 @@ const {info, contacts} = footerConfig
 export default function Footer() {
   const gridConfig = {
     base: 'grid-cols-10',
-    info: 'col-span-5',
-    navigation: 'col-span-2',
-    contacts: 'col-span-3',
+    info: 'col-span-5 xl:col-span-7',
+    navigation: 'col-span-2 xl:col-span-1',
+    contacts: 'col-span-3 xl:col-span-2',
   }
 
   return (
-    <footer className="py-16 space-y-32 xl:space-y-20 px-28 xl:px-16 sm:px-4 sm:py-8 bg-gray-light">
-      <div className={`grid gap-20 ${gridConfig.base} sm:flex sm:flex-col sm:gap-12`}>
+    <footer className="py-12 space-y-12 xl:py-10 px-28 xl:px-16 sm:px-4 sm:py-8 bg-gray-light">
+      <div className={`grid gap-20 ${gridConfig.base} sm:flex sm:flex-col sm:gap-10`}>
         <section className={`${gridConfig.info}`}>
           <div className="space-y-14 sm:space-y-7">
             <Heading type="h2" className="font-normal" text="Cras euismod porttitor ullamcorper. <br /> Vivamus vitae pellentesque justo." />
 
-            <div className="flex flex-wrap gap-5">
+            <div className="flex flex-wrap gap-5 sm:gap-3.5">
               {Object.entries(info).map(([department, email], index) => (
                 <div key={index} className="space-y-2 sm:w-full">
                   <Text type="sub" text={`${department}:`} />
-                  <Link href={`mailto:${email}`} className="flex items-center sm:justify-center gap-2 px-5 py-1.5 sm:w-full border-2 group w-fit border-gray">
+                  <Link href={`mailto:${email}`} className="flex items-center sm:justify-center gap-2 pl-4 pr-2.5 py-0.5 sm:w-full border-2 group w-fit border-gray hover:border-blue duration-200">
                     <Text type="p" className="mt-1 font-bold duration-200 group-hover:text-blue" text={email} />
                     <ArrowUpRight className="duration-200 group-hover:text-blue" size={35} strokeWidth={1.25} />
                   </Link>
@@ -50,10 +50,10 @@ export default function Footer() {
           </div>
         </section>
 
-        <section className={`space-y-5 ${gridConfig.navigation}`}>
+        <section className={`space-y-5 justify-self-end ${gridConfig.navigation}`}>
           <Text type="p" className="font-bold" text="Карта сайта" />
 
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 xl:gap-1.5">
             {Object.entries(siteRoutes).map(([key, label]) => (
               <Link className="hover:underline" href={`/${key}`} key={key}>
                 <Text type="p" text={label} />
@@ -62,10 +62,10 @@ export default function Footer() {
           </div>
         </section>
 
-        <section className={`space-y-5 ${gridConfig.contacts}`}>
+        <section className={`space-y-5 justify-self-end ${gridConfig.contacts}`}>
           <Text type="p" className="font-bold" text="Контакты" />
 
-          <div className="flex flex-col justify-between gap-5">
+          <div className="flex flex-col justify-between gap-3 xl:gap-1.5">
             {Object.entries(contacts).map(([label, link]) => (
               <Link className="hover:underline" href={link} key={link}>
                 <Text type="p" text={label} />
@@ -75,12 +75,12 @@ export default function Footer() {
         </section>
       </div>
 
-      <section className="flex items-end justify-between sm:items-start sm:flex-col-reverse sm:gap-4">
+      <section className="flex items-end justify-between sm:items-start sm:flex-col sm:gap-6">
         <Link className="hover:underline" href="/privacy-policy">
           <Text type="sub" text="Политика конфиденциальности" />
         </Link>
 
-        <Image className="object-contain h-[70px] xl:h-14 sm:h-7 w-fit" src={LogoImage} alt="Логтип Ориентир" />
+        <Image className="object-contain h-10 sm:h-7 w-fit" src={LogoImage} alt="Логтип Ориентир" />
       </section>
     </footer>
   )
