187dc5b5912360e9cf12b6d1b95a10e83db1a4c9
maxim <bozzhik@ya.ru>
2025-01-11 17:54:37 +0300

    stylize `Map` UI component

--- src/components/UI/Map.tsx
+++ src/components/UI/Map.tsx
@@ -5,14 +5,17 @@ import {YMaps, Map} from '@pbe/react-yandex-maps'
 type Props = {
   coordinates?: number[]
   zoom?: number
+  height?: string
 }
 
 const testPoint = [55.63567511574693, 38.05576231324671]
 
-export default function YandexMap({coordinates = testPoint, zoom = 16}: Props) {
+export default function YandexMap({coordinates = testPoint, zoom = 16, height = '65vh'}: Props) {
   return (
     <YMaps query={{lang: 'ru_RU'}}>
-      <Map className="w-full h-[60vh] saturate-0" defaultState={{center: coordinates, zoom: zoom}} />
+      <div className="relative w-full overflow-hidden" style={{height: `calc(${height} - 35px)`}}>
+        <Map className="absolute inset-0 w-full saturate-0" style={{height: height}} defaultState={{center: coordinates, zoom: zoom}} />
+      </div>
     </YMaps>
   )
 }
