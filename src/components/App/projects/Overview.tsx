7d5396f89b04d89ee8359cef254d6a0f8dc243f0
maxim <bozzhik@ya.ru>
2025-01-11 19:28:12 +0300

    init `Icons` UI component
    im tired of custom svg icons..

--- src/components/App/projects/Overview.tsx
+++ src/components/App/projects/Overview.tsx
@@ -9,16 +9,9 @@ import Heading from '~/UI/Heading'
 import Text from '~/UI/Text'
 import {DetailsButton} from '~/UI/Button'
 import YandexMap from '~/UI/Map'
+import {ArrowDownRight} from '~/UI/Icons'
 
 const projectStates: (ResidentStatus | 'Все')[] = ['Все', 'Завершен', 'В процессе', 'Свободные земельные участки']
-
-export function ArrowDownRight({className}: {className?: string}) {
-  return (
-    <svg className={className} width="39" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="m22.96 31.84-1.98-1.98 12.463-12.464H.597v-2.792h32.846L20.98 2.141 22.96.16 38.799 16l-15.84 15.838Z" />
-    </svg>
-  )
-}
 
 export default function Overview({items: projects}: {items: TProject[]}) {
   const [activeTab, setActiveTab] = useState<number | null>(0)
