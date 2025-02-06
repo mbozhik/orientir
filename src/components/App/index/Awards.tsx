0d3c16deddfbc229315c43af8484bafe94d65df7
maxim <bozzhik@ya.ru>
2025-02-06 22:58:59 +0300

    remodel `Awards` index block

--- src/components/App/index/Awards.tsx
+++ src/components/App/index/Awards.tsx
@@ -1,46 +1,46 @@
 import {cn} from '@/lib/utils'
-import {isMobile} from '@bozzhik/is-mobile'
 import {containerStyles} from '~/Global/Container'
-
 import Heading from '~/UI/Heading'
 import Text from '~/UI/Text'
 
 const awardsConfig = [
-  {
-    year: 19,
-    title: 'CRE Awards',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.',
-  },
-  {
-    year: 18,
-    title: 'CRE Awards',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.',
-  },
+  {year: 19, title: 'CRE Awards', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.']},
+  {year: 18, title: 'CRE Awards', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.']},
+  {year: 17, title: 'CRE Awards', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.']},
+  {year: 16, title: 'CRE Awards', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.']},
+  {year: 15, title: 'CRE Awards', description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper, metus sit amet imperdiet dignissim, lectus nunc imperdiet leo, mattis efficitur metus orci at enim. Praesent laoreet nulla et est malesuada vestibulum. Aliquam sodales posuere nunc vel mattis.']},
 ]
 
 const headingStyles = 'text-[220px] xl:text-[150px] sm:text-2xl sm:text-gray leading-none font-bold'
 
 export default function Awards() {
   return (
-    <section data-section="awards-index" className="space-y-20 sm:space-y-7">
+    <section data-section="awards-index" className="relative space-y-20 sm:space-y-7">
       <Heading className="max-w-[50ch]" type="h1" text="Награды" />
 
-      {(isMobile ? awardsConfig : awardsConfig.slice(0, 1)).map((award, index) => (
-        <div key={index} className={cn('flex sm:flex-col', containerStyles.min_width)}>
-          <h1 className={cn(headingStyles, 'sm:hidden')}>20</h1>
+      <div className={cn('flex', containerStyles.min_width)}>
+        <h1 className={cn(headingStyles, 'sticky top-52 self-start sm:hidden')}>20</h1>
 
-          <div className="flex justify-between w-full gap-32 border-b-2 sm:pb-3 sm:gap-0 sm:flex-col border-gray-light">
-            <h1 className={cn(headingStyles, 'sm:inline-flex')}>
-              <span className="hidden sm:inline">20</span> {award.year}
-            </h1>
+        <div className="flex flex-col">
+          {awardsConfig.map((award, index) => (
+            <div key={index} className="flex sm:flex-col">
+              <div className={cn('flex justify-between w-full gap-32 sm:pb-3 sm:gap-0 sm:flex-col border-b-2 border-gray-dark sm:border-gray-light', index !== 0 && 'pt-6', index === awardsConfig.length - 1 && 'border-transparent')}>
+                <h1 className={cn(headingStyles, 'sticky sm:static top-52 self-start sm:font-normal')}>
+                  <span className="hidden sm:inline">20</span>
+                  <span>{award.year}</span>
+                </h1>
 
-            <div className="mt-2 space-y-5 sm:space-y-0.5">
-              <Heading type="h2" text={award.title} />
-              <Text type="p" text={award.description} />
+                <div className="mt-2 space-y-5 sm:space-y-2 pb-20 sm:pb-4">
+                  <Heading type="h2" text={award.title} />
+                  {award.description.map((sentence, i) => (
+                    <Text key={i} type="p" text={sentence} />
+                  ))}
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
     </section>
   )
 }
