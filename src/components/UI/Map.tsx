3dd682b0ddbfe4fb01c7cfdbbd6279bc2dc99336
maxim <bozzhik@ya.ru>
2025-02-07 16:26:30 +0300

    finalize `Map` UI component

--- src/components/UI/Map.tsx
+++ src/components/UI/Map.tsx
@@ -14,24 +14,29 @@ export type YMapCoordinates = YMapLocationRequest & {
 type Props = {
   coordinates: YMapCoordinates
   placemarks?: YMapCoordinates[]
+  height?: string
 }
 
 const customYMapData: VectorCustomizationItem[] = CustomYMap as VectorCustomizationItem[]
 
-export function Map({coordinates, placemarks}: Props) {
+export function Map({coordinates, placemarks, height = '80vh'}: Props) {
   const mapPlacemarks: LngLat[] = placemarks?.length ? placemarks.map((placemark) => placemark.center) : [coordinates.center]
 
   return (
     <YMapComponentsProvider apiKey={process.env.NEXT_PUBLIC_YMAPS_API_KEY ?? ''} lang="ru_RU">
-      <YMap key="map" className="h-screen" location={coordinates} mode="vector" theme="dark">
-        <YMapDefaultSchemeLayer customization={customYMapData} />
-        <YMapDefaultFeaturesLayer />
-        <YMapListener />
+      <div className="relative w-full overflow-hidden" style={{height: `calc(${height} - 35px)`}}>
+        <div style={{height}}>
+          <YMap key="map" location={coordinates} mode="vector" theme="dark">
+            <YMapDefaultSchemeLayer customization={customYMapData} />
+            <YMapDefaultFeaturesLayer />
+            <YMapListener />
 
-        {mapPlacemarks.map((placemark, idx) => (
-          <YMapDefaultMarker coordinates={placemark} key={idx} />
-        ))}
-      </YMap>
+            {mapPlacemarks.map((placemark, idx) => (
+              <YMapDefaultMarker coordinates={placemark} key={idx} color="#0033a0" />
+            ))}
+          </YMap>
+        </div>
+      </div>
     </YMapComponentsProvider>
   )
 }
