cb3be0f7a5d0286fb1622687d080e9f497f63a02
maxim <bozzhik@ya.ru>
2025-02-10 17:11:19 +0300

    remodel `ClientsModule` index block using `framer-motion`
    https://ground.bossadizenith.me/docs/without-framer-motion/marquee

--- src/components/App/index/ClientsModule.tsx
+++ src/components/App/index/ClientsModule.tsx
@@ -1,35 +1,22 @@
 'use client'
 
-import {cn} from '@/lib/utils'
-import {useEffect, useRef} from 'react'
-import {horizontalLoop} from '@/utils/gsap/horizontal-loop'
 import {containerStyles} from '~/Global/Container'
+import {cn} from '@/lib/utils'
 
 import Image from 'next/image'
+import Marquee from '~/UI/Marquee'
 
 export default function ClientsModule({items}: {items: string[]}) {
-  const marqueeRef = useRef<HTMLDivElement | null>(null)
-
-  useEffect(() => {
-    if (!marqueeRef.current) return
-
-    const marqueeItems = marqueeRef.current.querySelectorAll('[data-marquee="item"]')
-
-    horizontalLoop(marqueeItems, {
-      repeat: -1,
-      speed: 2,
-      paddingRight: 0,
-    })
-  }, [])
-
   return (
     <div className={cn('border-t-[1px] border-gray-light', containerStyles.min_width)}>
-      <div ref={marqueeRef} className="relative flex overflow-hidden">
-        {items.map((logo, index) => (
-          <div data-marquee="item" className="w-[350px] xl:w-[300px] sm:w-[200px] py-8 shrink-0 flex items-center justify-center" key={index}>
-            <Image className="w-40 xl:w-36 object-contain hover:scale-[1.1] duration-300" src={logo} alt={`Client logo ${index + 1}`} width={150} height={50} />
-          </div>
-        ))}
+      <div className="relative flex overflow-hidden">
+        <Marquee className="bg-muted">
+          {items.map((logo, index) => (
+            <div data-marquee="item" className="py-8 shrink-0 flex items-center justify-center" key={index}>
+              <Image className="w-40 xl:w-36 object-contain hover:scale-[1.1] duration-300" src={logo} alt={`Client logo ${index + 1}`} width={150} height={50} />
+            </div>
+          ))}
+        </Marquee>
       </div>
     </div>
   )
