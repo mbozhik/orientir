b2a14180f7c2f4279fb4043944f1c6d3ee65da8c
maxim <bozzhik@ya.ru>
2025-02-16 16:54:03 +0300

    fix `Info` projects block

--- src/components/App/projects/slug/Info.tsx
+++ src/components/App/projects/slug/Info.tsx
@@ -10,7 +10,7 @@ export default function Info({project}: {project: TProjectExtra}) {
     <section data-section="info-project" className="space-y-7 sm:space-y-5">
       <H1 className="mt-10 sm:hidden">{project.project}</H1>
 
-      <div className="grid grid-cols-2 sm:grid-cols-1 gap-14 xl:gap-10 sm:gap-7 px-44 xl:px-24 sm:px-0">
+      <div className="grid grid-cols-2 sm:flex sm:flex-col gap-14 xl:gap-10 sm:gap-7 px-44 xl:px-24 sm:px-0">
         {project.full_description && Array.isArray(project.full_description) ? project.full_description.map((text, index) => <H4 key={index}>{text}</H4>) : <H4 className="col-span-2">{project.full_description}</H4>}
 
         <div className="space-y-1.5 sm:space-y-5">
