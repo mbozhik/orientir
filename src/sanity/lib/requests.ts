f96d24ea984a68e3aa092fb7488c2e72437df725
maxim <bozzhik@ya.ru>
2025-04-25 18:40:30 +0300

    init `requests` file for `sanity` entities

---
+++ src/sanity/lib/requests.ts
@@ -0,0 +1,18 @@
+import {sanityFetch} from '@/sanity/lib/live'
+import {defineQuery} from 'next-sanity'
+
+export async function getDirections() {
+  const DIRECTIONS_QUERY = defineQuery(`
+    *[_type == "direction"]{
+        id, heading, params
+    }`)
+
+  try {
+    const directions = await sanityFetch({query: DIRECTIONS_QUERY})
+
+    return directions.data || []
+  } catch (error) {
+    console.log('Error fetching directions:', error)
+    return []
+  }
+}
