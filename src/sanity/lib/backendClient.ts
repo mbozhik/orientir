214650ab40a85a07a0625fe6d4623d96e41217f9
maxim <bozzhik@ya.ru>
2025-04-25 18:39:46 +0300

    configure `client` for `sanity` data

--- src/sanity/lib/backendClient.ts
+++ src/sanity/lib/backendClient.ts
@@ -1,14 +1,11 @@
 import {createClient} from 'next-sanity'
 
 import {apiVersion, dataset, projectId} from '@/sanity/env'
-import {API_URL} from '@/lib/constants'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-  stega: {
-    studioUrl: `${API_URL}/studio`,
-  },
+  token: process.env.NEXT_SANITY_TOKEN,
 })
