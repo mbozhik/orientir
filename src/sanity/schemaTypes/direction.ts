01e47d8d06e969970ade711be98396c7ea679e77
maxim <bozzhik@ya.ru>
2025-04-25 18:14:59 +0300

    init `direction` sanity entity

---
+++ src/sanity/schemaTypes/direction.ts
@@ -0,0 +1,41 @@
+import {DashboardIcon} from '@sanity/icons'
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const direction = defineType({
+  name: 'direction',
+  title: 'Направление',
+  type: 'document',
+  icon: DashboardIcon,
+  fields: [
+    defineField({
+      name: 'id',
+      type: 'number',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'heading',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'params',
+      title: 'Параметры',
+      type: 'array',
+      of: [
+        defineArrayMember({
+          name: 'item',
+          title: 'Параметр',
+          type: 'string',
+          validation: (rule) => rule.required(),
+        }),
+      ],
+      validation: (Rule) => Rule.min(1),
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'heading',
+      subtitle: 'id',
+    },
+  },
+})
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -3,13 +3,12 @@ import type {StructureResolver} from 'sanity/structure'
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('Ориентир CMS')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
+      S.documentTypeListItem('direction').title('Направление'),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
+        (item) => item.getId() && !['direction'].includes(item.getId()!),
       ),
     ])
+    
